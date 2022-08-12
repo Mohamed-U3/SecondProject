8232d4478c55c51d5aab43b1f1a3dd5e498973a0
Mohamed Yousry <engineer.mo.u3@gmail.com>
2022-08-12 22:39:00 +0200

    Feature added: now there is runner file where I can run the test on each featuer file on only one click (the Whole test Scenarios)

--- src/test/java/org/example/stepDefs/D01_registerStepDef.java
+++ src/test/java/org/example/stepDefs/D01_registerStepDef.java
@@ -1,13 +1,21 @@
 package org.example.stepDefs;
 
 import io.cucumber.java.en.Given;
+import org.example.pages.P01_register;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class D01_registerStepDef
 {
+    P01_register page = new P01_register();
+
     @Given("user go to register page")
     public void register_page()
     {
-        Hooks.driver.findElement(By.className("ico-register")).click();
+        // Hooks.driver.findElement(By.className("ico-register")).click();
+        page.registerBtn.click();
     }
+
+
 }
